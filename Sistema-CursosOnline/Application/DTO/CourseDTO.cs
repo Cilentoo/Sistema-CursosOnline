d46fbdcac7bbe7da2120eedfe94b37eb0a75b229
eduar <cilento762@gmail.com>
2025-01-10 01:52:02 -0300

    Iniciando Services

--- Sistema-CursosOnline/Application/DTO/CourseDTO.cs
+++ Sistema-CursosOnline/Application/DTO/CourseDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace Sistema_CursosOnline.Application.DTO
+﻿using Sistema_CursosOnline.Domain.Entities;
+
+namespace Sistema_CursosOnline.Application.DTO
 {
-    public record CourseDTO(int Id, string Title, string Description, int InstructorId, string Status, DateTime CreationDate);
+    public record CourseDTO(int Id, string Title, string Description, User InstructorId, string Status, DateTime CreationDate);
 }
